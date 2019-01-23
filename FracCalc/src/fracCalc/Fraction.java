30c5200ab7bb6f87bffa9841251144a8ea781ac9
Ryantong <20tongr31@mymhs.org>
2019-01-23 11:30:16 -0800

    uh quick message bai

---
+++ FracCalc/src/fracCalc/Fraction.java
@@ -0,0 +1,28 @@
+package fracCalc;
+
+public class Fraction {
+	private int whole;
+	private int numerator;
+	private int denominator;
+	
+	public String toString(Fraction answer) {
+		int[] 
+	}
+	
+	private Fraction toImproperFrac(Fraction op1) {
+		
+	}
+	public Fraction doMath(String operator, Fraction op2) {
+		
+	}
+	
+	Fraction(String op){
+		
+	}
+	
+	Fraction(){
+		whole = 0;
+		numerator = 0;
+		denominator = 0;
+	}
+}
